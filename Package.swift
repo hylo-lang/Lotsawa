104efa4cb6024b06542a8228e8ff276283c00b03
Dave Abrahams <dabrahams@adobe.com>
2023-12-29 16:22:01 -0800

    Attempt to pull the latest doc generation stuff from hylo

--- Package.swift
+++ Package.swift
@@ -1,10 +1,19 @@
 // swift-tools-version: 5.7
 import PackageDescription
+import Foundation
 
 let CitronParser
   = Target.Dependency.product(name: "CitronParserModule", package: "citron")
 let CitronLexer
   = Target.Dependency.product(  name: "CitronLexerModule", package: "citron")
+
+/// Dependencies for documentation extraction.
+///
+/// Most people don't need to extract documentation; set `HYLO_ENABLE_DOC_GENERATION` in your
+/// environment if you do.
+let docGenerationDependency: [Package.Dependency] =
+  ProcessInfo.processInfo.environment["LOTSAWA_ENABLE_DOC_GENERATION"] != nil
+  ? [.package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.1.0")] : []
 
 let package = Package(
   name: "Lotsawa",
@@ -20,7 +29,9 @@ let package = Package(
       .package(url: "https://github.com/dabrahams/citron.git", from: "2.1.1"),
       .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.1.0"),
       .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "0.12.0")
-    ],
+    ]
+      + docGenerationDependency,
+
     targets: [
         .target(
             name: "Lotsawa",
